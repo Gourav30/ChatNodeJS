3320b058ccc56aa3a08eb64ff48d87d5b71749fc
Gourav <gouravk3094@gmail.com>
2020-05-28 11:09:57 +0530

    third commit

---
+++ app/library/socketLib.js
@@ -0,0 +1,82 @@
+/**
+ * modules dependencies.
+ */
+const socketio = require('socket.io');
+const mongoose = require('mongoose');
+const shortid = require('shortid');
+const logger = require('./logLib');
+const events = require('events');
+const eventEmitter = new events.EventEmitter();
+
+const tokenLib = require("./tokenLib");
+const check = require("./checkLib");
+const response = require('./responseLib')
+
+let setServer = (server) => {
+
+    let allOnlineUsers = []
+
+    let io = socketio.listen(server);
+
+    let myIo = io.of('')
+
+    myIo.on('connection',(socket) => {
+
+        console.log("on connection--emitting verify user");
+
+        socket.emit("verifyUser", "");
+
+        // code to verify the user and make him online
+
+        socket.on('set-user',(authToken) => {
+
+            console.log("set-user called")
+            tokenLib.verifyClaimWithoutSecret(authToken,(err,user)=>{
+                if(err){
+                    socket.emit('auth-error', { status: 500, error: 'Please provide correct auth token' })
+                }
+                else{
+
+                    console.log("user is verified..setting details");
+                    let currentUser = user.data;
+                    // setting socket user id
+                    socket.userId = currentUser.userId
+                    let fullName = `${currentUser.firstName} ${currentUser.lastName}`
+                    console.log(`${fullName} is online`);
+                    socket.emit(currentUser.userId,"You are online")
+
+                    let userObj = {userId:currentUser.userId,fullName:fullName}
+                    allOnlineUsers.push(userObj)
+                    console.log(allOnlineUsers)
+
+                }
+
+
+            })
+
+        }) // end of listening set-user event
+
+
+        socket.on('disconnect', () => {
+            // disconnect the user from socket
+            // remove the user from online list
+            // unsubscribe the user from his own channel
+
+            console.log("user is disconnected");
+            // console.log(socket.connectorName);
+            console.log(socket.userId);
+            var removeIndex = allOnlineUsers.map(function(user) { return user.userId; }).indexOf(socket.userId);
+            allOnlineUsers.splice(removeIndex,1)
+            console.log(allOnlineUsers)
+
+
+        }) // end of on disconnect
+
+
+    });
+
+}
+
+module.exports = {
+    setServer: setServer
+}
